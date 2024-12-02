8ece81986b0845347ad3b28953978287963e0c86
LeeJaeHyun <myleje@gmail.com>
2024-12-02 10:13:01 +0900

    .

--- Assets/02_Scripts/AuthManager.cs
+++ Assets/02_Scripts/AuthManager.cs
@@ -1,9 +1,22 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Services.Core;
 
 public class AuthManager : MonoBehaviour
 {
     [SerializeField] private Button signInButton;
     [SerializeField] private TMP_Text messageText;
+
+    private async void Awake()
+    {
+        // UGS 초기화 콜백
+        UnityServices.Initialized += () =>
+        {
+            messageText.text += "UnityServices Init! \n";
+        };
+
+        // UGS 초기화
+        await UnityServices.InitializeAsync();
+    }
 }
