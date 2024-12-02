0dbfc5f3ccd7e39022905a874116b302b20072ad
LeeJaeHyun <myleje@gmail.com>
2024-12-02 12:46:13 +0900

    .

--- Assets/02_Scripts/UserNamePassword/AuthManager.cs
+++ Assets/02_Scripts/UserNamePassword/AuthManager.cs
@@ -1,9 +1,12 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UserNamePassword
 {
     public class AuthManager : MonoBehaviour
     {
-
+        [SerializeField] private TMP_InputField userNameIf, passwordIf;
+        [SerializeField] private Button signUpButton, signInButton;
     }
 }
