eda5bd14365f69ee749d62a416fe9443784ecd30
LeeJaeHyun <myleje@gmail.com>
2024-12-02 14:41:00 +0900

    .

--- Assets/02_Scripts/CloudSave/CloudSaveManager.cs
+++ Assets/02_Scripts/CloudSave/CloudSaveManager.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.CloudSave;
 using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,5 +23,31 @@ public class CloudSaveManager : MonoBehaviour
 
         // 익명로그인 요청
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        // 버튼 이벤트 연결
+        BindingEvents();
     }
+
+    private void BindingEvents()
+    {
+        singleDataSaveButton.onClick.AddListener(async () => await SaveSingleDataAsync());
+    }
+
+    #region 싱글데이터 저장
+    public async Task SaveSingleDataAsync()
+    {
+        // 저장할 데이터를 정의
+        var data = new Dictionary<string, object>
+        {
+            {"player_name", "Zackiller"},
+            {"level", 30},
+            {"xp", 2000},
+            {"gold", 100}
+        };
+
+        // 저장 요청
+        await CloudSaveService.Instance.Data.Player.SaveAsync(data);
+        Debug.Log("데이터 저장 완료");
+    }
+    #endregion
 }
