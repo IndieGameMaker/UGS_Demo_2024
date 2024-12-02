20650c8b58fade4b59b29b6368e77a73d6fe1bda
LeeJaeHyun <myleje@gmail.com>
2024-12-02 10:20:34 +0900

    .

--- Assets/02_Scripts/AuthManager.cs
+++ Assets/02_Scripts/AuthManager.cs
@@ -2,6 +2,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Services.Core;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
 
 public class AuthManager : MonoBehaviour
 {
@@ -18,5 +20,22 @@ public class AuthManager : MonoBehaviour
 
         // UGS 초기화
         await UnityServices.InitializeAsync();
+
+        // 버튼 이벤트 연결
+        signInButton.onClick.AddListener(async () => await SignInAsync());
+    }
+
+    // 익명 로그인 메소드
+    private async Task SignInAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("익명 로그인 성공");
+        }
+        catch (AuthenticationException e)
+        {
+            Debug.Log(e.Message);
+        }
     }
 }
