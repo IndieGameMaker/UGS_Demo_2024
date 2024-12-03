52b1c0500a287f9dc6c7b5e8a980e6a639d23a08
LeeJaeHyun <myleje@gmail.com>
2024-12-03 12:28:39 +0900

    .

--- Assets/02_Scripts/RemoteConfigManager.cs
+++ Assets/02_Scripts/RemoteConfigManager.cs
@@ -1,5 +1,7 @@
+using System.Threading.Tasks;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using Unity.Services.RemoteConfig;
 using UnityEngine;
 
 public class RemoteConfigManager : MonoBehaviour
@@ -18,5 +20,25 @@ public class RemoteConfigManager : MonoBehaviour
         };
 
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        // Remote Config 이벤트 연결
+        RemoteConfigService.Instance.FetchCompleted += _ =>
+        {
+            moveSpeed = RemoteConfigService.Instance.appConfig.GetFloat("move_speed");
+            turnSpeed = RemoteConfigService.Instance.appConfig.GetFloat("turn_speed");
+            scale = RemoteConfigService.Instance.appConfig.GetInt("scale");
+
+            GameObject.Find("Mummy_Mon").transform.localScale = Vector3.one * scale;
+        };
+
+        await GetRemoteConfigData();
+    }
+
+    private struct userAtt { };
+    private struct appAtt { };
+
+    private async Task GetRemoteConfigData()
+    {
+        await RemoteConfigService.Instance.FetchConfigsAsync(new userAtt(), new appAtt());
     }
 }
