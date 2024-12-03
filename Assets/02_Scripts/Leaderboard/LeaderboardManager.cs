712b4dfae61b55c958f107bd3891a7fd2c948b7d
LeeJaeHyun <myleje@gmail.com>
2024-12-03 10:36:11 +0900

    .

--- Assets/02_Scripts/Leaderboard/LeaderboardManager.cs
+++ Assets/02_Scripts/Leaderboard/LeaderboardManager.cs
@@ -1,6 +1,10 @@
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using Unity.Services.Leaderboards;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,9 +13,25 @@ public class LeaderboardManager : MonoBehaviour
     [SerializeField] private TMP_InputField scoreIf;
     [SerializeField] private Button scoreSaveButton;
 
+    private const string LEADERBOARD_ID = "TopRank";
+
     private async void Awake()
     {
         await UnityServices.InitializeAsync();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        scoreSaveButton.onClick.AddListener(async () =>
+        {
+            int score = int.Parse(scoreIf.text);
+            await AddScore(score);
+        });
+    }
+
+    // 점수 기록
+    private async Task AddScore(int score)
+    {
+        var response = await LeaderboardsService.Instance.AddPlayerScoreAsync(LEADERBOARD_ID, score);
+        var result = JsonConvert.SerializeObject(response);
+        Debug.Log(result);
     }
 }
