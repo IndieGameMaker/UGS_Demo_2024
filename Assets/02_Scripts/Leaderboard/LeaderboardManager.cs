68b52bafffbe0f64062611c34d6b5ea6e91afeb1
LeeJaeHyun <myleje@gmail.com>
2024-12-03 10:29:10 +0900

    .

--- Assets/02_Scripts/Leaderboard/LeaderboardManager.cs
+++ Assets/02_Scripts/Leaderboard/LeaderboardManager.cs
@@ -1,9 +1,14 @@
+using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeaderboardManager : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField scoreIf;
+    [SerializeField] private Button scoreSaveButton;
+
     private async void Awake()
     {
         await UnityServices.InitializeAsync();
